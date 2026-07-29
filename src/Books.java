85805b45a6160ca692a7a7e484e30bd1afb76fdc
aleksantaaa <sasha2668mus@gmail.com>
2026-07-30 01:17:11 +0200

    Update Books.java

--- src/Books.java
+++ src/Books.java
@@ -1,4 +1,8 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 class Book {
 
@@ -27,23 +31,77 @@ class Library {
 
     void showBooks() {
 
-        for (int i=0; i < books.size(); i++) {
-            System.out.println( books.get(i).title + " - " + books.get(i).author );
+        for (int i = 0; i < books.size(); i++) {
+            System.out.println(books.get(i).title + " - " + books.get(i).author);
         }
     }
-}
+        void saveToFile (String filename) {
 
-public class Books {
+            try {
 
-    public static void main(String[] args) {
+                FileWriter writer = new FileWriter(filename);
 
-        Library l = new Library();
+                for (int i=0;  i < books.size(); i++) {
 
-        l.addBook("Гарри Поттер 1", "Джоан Роулинг");
-        l.addBook("Гарри Поттер 2", "Джоан Роулинг");
-        l.addBook("Гарри Поттер 3", "Джоан Роулинг");
+                    writer.write( books.get(i).title + ";" + books.get(i).author + "\n");
 
-        l.showBooks();
+                }
 
+                writer.close();
+
+            } catch ( IOException e) {
+
+                System.out.println("There is a failure with saving: " + e.getMessage());
+
+            }
+        }
+
+        void loadFromFile (String filename) {
+
+            try {
+
+                File file = new File(filename);
+                Scanner reader = new Scanner(file);
+
+
+                while ( reader.hasNextLine()) {
+
+                    String line = reader.nextLine();
+
+                    String[] parts = line.split(";");
+
+                    Book newBook = new Book(parts[0], parts[1]);
+                    books.add(newBook);
+
+                }
+
+            } catch (IOException e) {
+
+                System.out.println("There is a problem with loading the file: " + e.getMessage());
+
+            }
+        }
     }
-}
+
+
+
+
+
+
+    public class Books {
+
+        public static void main(String[] args) {
+
+            Library l = new Library();
+
+            l.addBook("Гарри Поттер 1", "Джоан Роулинг");
+            l.addBook("Гарри Поттер 2", "Джоан Роулинг");
+            l.addBook("Гарри Поттер 3", "Джоан Роулинг");
+
+            l.saveToFile("library.txt");
+            l.loadFromFile("library.txt");
+            l.showBooks();
+
+        }
+    }
+
